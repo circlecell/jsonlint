03b03c846900b76f6e9ae173351a6ff8d0a22e1f
Build Interactive <2158752+buildinteractive@users.noreply.github.com>
2023-09-29 17:50:50 -0400

    Button spacing in mobile

--- components/jsonlint.js
+++ components/jsonlint.js
@@ -114,8 +114,8 @@ const JSONLint = ({ json, url }) => {
 		</div>
 		<div>
 			<button className="py-3 px-4 inline-flex mt-6 justify-center mr-4 items-center gap-2 border-2 border-green-500 text-white bg-green-500 font-semibold text-green-500 hover:text-white hover:bg-green-500 hover:border-green-500 focus:outline-none focus:ring-2 focus:ring-green-200 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800" onClick={() => handleValidate()}>Validate JSON</button>
-			<button className="py-3 px-4 inline-flex justify-center mr-4 items-center gap-2 border-2 border-slate-200 font-semibold text-slate-500 hover:text-white hover:bg-slate-500 hover:border-slate-500 focus:outline-none focus:ring-2 focus:ring-slate-200 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-slate-800" onClick={handleClear}>Clear</button>
-			<button className="py-3 px-4 inline-flex justify-center items-center gap-2 border-2 border-slate-200 font-semibold text-slate-500 hover:text-white hover:bg-slate-500 hover:border-slate-500 focus:outline-none focus:ring-2 focus:ring-slate-200 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-slate-800" onClick={handleFormatting}>{isPretty ? "Compress" : "Prettify"}</button>
+			<button className="py-3 px-4 inline-flex mt-6 justify-center mr-4 items-center gap-2 border-2 border-slate-200 font-semibold text-slate-500 hover:text-white hover:bg-slate-500 hover:border-slate-500 focus:outline-none focus:ring-2 focus:ring-slate-200 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-slate-800" onClick={handleClear}>Clear</button>
+			<button className="py-3 px-4 inline-flex mt-6 justify-center items-center gap-2 border-2 border-slate-200 font-semibold text-slate-500 hover:text-white hover:bg-slate-500 hover:border-slate-500 focus:outline-none focus:ring-2 focus:ring-slate-200 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-slate-800" onClick={handleFormatting}>{isPretty ? "Compress" : "Prettify"}</button>
 			
 			{isValid === true && 
 			    <div className="bg-green-100 border border-green-200 text-green-700 px-4 py-2 mt-4">
