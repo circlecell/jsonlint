f7f421ee5a7231c17cb352f29cd975b20ca73533
Todd Garland <todd@toddgarland.com>
2023-10-03 08:26:52 -0400

    Added search, gutter collapse, and copy to clipboard

--- components/jsonlint.js
+++ components/jsonlint.js
@@ -1,10 +1,27 @@
 import React, { useState, useEffect, useRef, useContext } from 'react'
 import ValidContext from '../contexts/ValidContext'
 import jsonlint from 'jsonlint-mod'
+import copy from 'copy-to-clipboard'
 import { Controlled as CodeMirror } from 'react-codemirror2'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/dracula.css'
 import 'codemirror/mode/javascript/javascript'
+
+// Fold
+import 'codemirror/addon/fold/foldcode'
+import 'codemirror/addon/fold/foldgutter'
+import 'codemirror/addon/fold/brace-fold'
+import 'codemirror/addon/fold/foldgutter.css'
+
+// Search
+import 'codemirror/addon/dialog/dialog.css'
+import 'codemirror/addon/search/matchesonscrollbar.css'
+import 'codemirror/addon/dialog/dialog'
+import 'codemirror/addon/search/searchcursor'
+import 'codemirror/addon/search/search'
+import 'codemirror/addon/scroll/annotatescrollbar'
+import 'codemirror/addon/search/matchesonscrollbar'
+import 'codemirror/addon/search/jump-to-line'
 
 const JSONLint = ({ json, url }) => {
 	const { isValid, setIsValid } = useContext(ValidContext)
@@ -14,6 +31,7 @@ const JSONLint = ({ json, url }) => {
 	const [error, setError] = useState('')
 	const editorRef = useRef(null)
 	const errorMarkerRef = useRef(null)
+	const [copySuccess, setCopySuccess] = useState(false)
 
 	const handleValidate = (jsonToValidate = input) => {
 		if (errorMarkerRef.current) {
@@ -75,6 +93,12 @@ const JSONLint = ({ json, url }) => {
 		}
 	}
 	
+	const handleCopy = () => {
+		copy(input)
+		setCopySuccess(true)
+		setTimeout(() => setCopySuccess(false), 2000)
+	}
+	
 	useEffect(() => {
 	    if (json) {
 	        const decodedJson = decodeURIComponent(json)
@@ -97,12 +121,18 @@ const JSONLint = ({ json, url }) => {
 
 	return (
 		<>
-		<div className="border border-slate-200 dark:border-slate-600">
+		<div className="relative border border-slate-200 dark:border-slate-600">
 			<CodeMirror
 				value={input}
 				options={{
 					mode: { name: "javascript", json: true },
 					lineNumbers: true,
+					foldGutter: true,
+					gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+					matchBrackets: true,
+					autoCloseBrackets: true,
+					search: true,
+					highlightSelectionMatches: {showToken: /\w/, annotateScrollbar: true}
 				}}
 				editorDidMount={editor => {
 					editorRef.current = editor
@@ -111,6 +141,31 @@ const JSONLint = ({ json, url }) => {
 					setInput(value)
 				}}
 			/>
+			<button 
+			    className="absolute top-2 right-6 p-1 bg-slate-50 hover:bg-slate-100 border border-slate-200 text-slate-400" 
+			    onClick={handleCopy} 
+			    title="Copy to clipboard"
+			>
+			    {copySuccess ? (
+	                // Checkmark SVG
+					<svg width="24" height="24" className="px-1 py-1 text-green-500" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+						<path 
+							fill="currentColor" 
+							stroke="currentColor" 
+							d="M480 128c0 8.188-3.125 16.38-9.375 22.62l-256 256C208.4 412.9 200.2 416 192 416s-16.38-3.125-22.62-9.375l-128-128C35.13 272.4 32 264.2 32 256c0-18.28 14.95-32 32-32c8.188 0 16.38 3.125 22.62 9.375L192 338.8l233.4-233.4C431.6 99.13 439.8 96 448 96C465.1 96 480 109.7 480 128z"
+						/>
+					</svg>
+	            ) : (
+	                // Copy SVG
+	                <svg width="24" height="24" className="px-1 py-1" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+				        <path 
+				            fill="currentColor" 
+				            stroke="currentColor" 
+				            d="M448 0H224C188.7 0 160 28.65 160 64v224c0 35.35 28.65 64 64 64h224c35.35 0 64-28.65 64-64V64C512 28.65 483.3 0 448 0zM464 288c0 8.822-7.178 16-16 16H224C215.2 304 208 296.8 208 288V64c0-8.822 7.178-16 16-16h224c8.822 0 16 7.178 16 16V288zM304 448c0 8.822-7.178 16-16 16H64c-8.822 0-16-7.178-16-16V224c0-8.822 7.178-16 16-16h64V160H64C28.65 160 0 188.7 0 224v224c0 35.35 28.65 64 64 64h224c35.35 0 64-28.65 64-64v-64h-48V448z"
+				        />
+				    </svg>
+	            )}
+			</button>
 		</div>
 		<div>
 			<button className="py-3 px-4 inline-flex mt-6 justify-center mr-4 items-center gap-2 border-2 border-green-500 text-white bg-green-500 font-semibold text-green-500 hover:text-white hover:bg-green-500 hover:border-green-500 focus:outline-none focus:ring-2 focus:ring-green-200 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800" onClick={() => handleValidate()}>Validate JSON</button>
