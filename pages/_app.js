44556472848335391049814d2f46855a3dd3c971
Todd Garland <todd@toddgarland.com>
2024-06-06 10:20:11 -0400

    cloudflare worker vs fullres url

--- pages/_app.js
+++ pages/_app.js
@@ -23,7 +23,7 @@ export default function App({ Component, pageProps }) {
 			}
 			const newScript = document.createElement('script');
 			newScript.async = true;
-			newScript.src = 'https://t.fullres.net/jsonlint.js?' + (new Date() - new Date() % 43200000);
+			newScript.src = 'https://jsonlint.com/script.js?' + (new Date() - new Date() % 43200000);
 			newScript.id = 'fullres';
 			document.head.appendChild(newScript);
 		}
